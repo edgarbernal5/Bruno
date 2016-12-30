fe65fd5de011c960e3d84b9af5613d9a9c7f5a6c
Edgar <edgar.bernal@gmail.com>
2016-12-30 19:58:02 -0400

    - Modify the header for precompilation.

--- TrioEngine/TrioAPI/stdafx.h
+++ TrioEngine/TrioAPI/stdafx.h
@@ -5,13 +5,53 @@
 
 #pragma once
 
-#include "targetver.h"
+#pragma warning(push)
+#pragma warning(disable : 4005)
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#define NODRAWTEXT
+#define NOGDI
+#define NOBITMAP
+#define NOMCX
+#define NOSERVICE
+#define NOHELP
+#pragma warning(pop)
 
-#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
-// Windows Header Files:
 #include <windows.h>
 
+#ifndef _WIN32_WINNT_WIN10
+#define _WIN32_WINNT_WIN10 0x0A00
+#endif
 
+#if defined(_XBOX_ONE) && defined(_TITLE)
+#include <d3d11_x.h>
+#define DCOMMON_H_INCLUDED
+#else
+#include <d3d11_1.h>
+#endif
 
-// TODO: reference additional headers your program requires here
+#if defined(WINAPI_FAMILY) && (WINAPI_FAMILY == WINAPI_FAMILY_APP)
+#include <Windows.UI.Core.h>
+#endif
 
+#include <DirectXMath.h>
+#include <DirectXPackedVector.h>
+#include <DirectXCollision.h>
+
+#include <algorithm>
+#include <array>
+#include <exception>
+#include <list>
+#include <malloc.h>
+#include <map>
+#include <memory>
+#include <set>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include <stdint.h>
+
+#include <wrl.h>
+
+#include <wincodec.h>
