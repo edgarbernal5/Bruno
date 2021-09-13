bd3628f76e0db26d76730c410ff59f3a3903b01a
Edgar Bernal <edgar.bernal@gmail.com>
2021-09-12 23:24:51 -0300

    Fixed scale colors when selects an axis

--- TrioEngine/BrunoFramework/Graphics/Editor/Gizmos/AxisGizmoGraphics.cs
+++ TrioEngine/BrunoFramework/Graphics/Editor/Gizmos/AxisGizmoGraphics.cs
@@ -42,36 +42,36 @@ namespace BrunoFramework.Graphics.Editor
             switch (gizmoAxis)
             {
                 case GizmoAxis.X:
-                    m_translationLinesVtx[0] = new VertexPositionColor(Vector3.Zero, m_color[0]);
-                    m_translationLinesVtx[1] = new VertexPositionColor(new Vector3(lineLength, 0.0f, 0.0f), m_color[0]);
+                    m_translationLinesVtx[0].Position = Vector3.Zero;
+                    m_translationLinesVtx[1].Position = new Vector3(lineLength, 0, 0);
 
-                    m_translationLinesVtx[2] = new VertexPositionColor(new Vector3(m_lineOffset, 0.0f, 0.0f), m_color[0]);
-                    m_translationLinesVtx[3] = new VertexPositionColor(new Vector3(m_lineOffset, 0.0f, m_lineOffset), m_color[0]);
+                    m_translationLinesVtx[2].Position = new Vector3(m_lineOffset, 0.0f, 0.0f);
+                    m_translationLinesVtx[3].Position = new Vector3(m_lineOffset, 0.0f, m_lineOffset);
 
-                    m_translationLinesVtx[4] = new VertexPositionColor(new Vector3(m_lineOffset, 0.0f, 0.0f), m_color[0]);
-                    m_translationLinesVtx[5] = new VertexPositionColor(new Vector3(m_lineOffset, m_lineOffset, 0.0f), m_color[0]);
+                    m_translationLinesVtx[4].Position = new Vector3(m_lineOffset, 0.0f, 0.0f);
+                    m_translationLinesVtx[5].Position = new Vector3(m_lineOffset, m_lineOffset, 0.0f);
 
                     break;
                 case GizmoAxis.Y:
-                    m_translationLinesVtx[6] = new VertexPositionColor(Vector3.Zero, m_color[1]);
-                    m_translationLinesVtx[7] = new VertexPositionColor(new Vector3(0.0f, lineLength, 0.0f), m_color[1]);
+                    m_translationLinesVtx[6].Position = Vector3.Zero;
+                    m_translationLinesVtx[7].Position = new Vector3(0.0f, lineLength, 0.0f);
 
-                    m_translationLinesVtx[8] = new VertexPositionColor(new Vector3(0.0f, m_lineOffset, 0.0f), m_color[1]);
-                    m_translationLinesVtx[9] = new VertexPositionColor(new Vector3(m_lineOffset, m_lineOffset, 0.0f), m_color[1]);
+                    m_translationLinesVtx[8].Position = new Vector3(0.0f, m_lineOffset, 0.0f);
+                    m_translationLinesVtx[9].Position = new Vector3(m_lineOffset, m_lineOffset, 0.0f);
 
-                    m_translationLinesVtx[10] = new VertexPositionColor(new Vector3(0.0f, m_lineOffset, 0.0f), m_color[1]);
-                    m_translationLinesVtx[11] = new VertexPositionColor(new Vector3(0.0f, m_lineOffset, m_lineOffset), m_color[1]);
+                    m_translationLinesVtx[10].Position = new Vector3(0.0f, m_lineOffset, 0.0f);
+                    m_translationLinesVtx[11].Position = new Vector3(0.0f, m_lineOffset, m_lineOffset);
 
                     break;
                 case GizmoAxis.Z:
-                    m_translationLinesVtx[12] = new VertexPositionColor(Vector3.Zero, m_color[2]);
-                    m_translationLinesVtx[13] = new VertexPositionColor(new Vector3(0.0f, 0.0f, lineLength), m_color[2]);
+                    m_translationLinesVtx[12].Position = Vector3.Zero;
+                    m_translationLinesVtx[13].Position = new Vector3(0.0f, 0.0f, lineLength);
 
-                    m_translationLinesVtx[14] = new VertexPositionColor(new Vector3(0.0f, 0.0f, m_lineOffset), m_color[2]);
-                    m_translationLinesVtx[15] = new VertexPositionColor(new Vector3(0.0f, m_lineOffset, m_lineOffset), m_color[2]);
+                    m_translationLinesVtx[14].Position=new Vector3(0.0f, 0.0f, m_lineOffset);
+                    m_translationLinesVtx[15].Position = new Vector3(0.0f, m_lineOffset, m_lineOffset);
 
-                    m_translationLinesVtx[16] = new VertexPositionColor(new Vector3(0.0f, 0.0f, m_lineOffset), m_color[2]);
-                    m_translationLinesVtx[17] = new VertexPositionColor(new Vector3(m_lineOffset, 0.0f, m_lineOffset), m_color[2]);
+                    m_translationLinesVtx[16].Position = new Vector3(0.0f, 0.0f, m_lineOffset);
+                    m_translationLinesVtx[17].Position = new Vector3(m_lineOffset, 0.0f, m_lineOffset);
 
                     break;
                 
