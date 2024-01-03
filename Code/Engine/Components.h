b63b1fd9cdb22bf9a17feca821c0464a64955f4a
edgarbernal5 <edgar.bernal@gmail.com>
2024-01-03 00:09:26 -0300

    Added IdComponent, HierarchyComponent

--- Code/Engine/Components.h
+++ Code/Engine/Components.h
@@ -1,13 +1,31 @@
 #pragma once
 
 #include "Bruno/Math/Math.h"
+#include <vector>
 
 namespace Bruno
 {
+	using UUID = uint64_t;
+
+	struct IdComponent
+	{
+		UUID Id{ 0 };
+	};
+
+	struct HierarchyComponent
+	{
+		UUID Parent{ 0 };
+		std::vector<UUID> Children;
+
+		HierarchyComponent() = default;
+		HierarchyComponent(const HierarchyComponent& other) = default;
+		HierarchyComponent(UUID parent) : Parent(parent) {}
+	};
+
 	struct TransformComponent
 	{
 		Math::Vector3 Position;
-		Math::Vector3 Scale{ 1.0f, 1.0f,1.0f };
+		Math::Vector3 Scale{ 1.0f, 1.0f, 1.0f };
 		Math::Quaternion Rotation;
 
 		TransformComponent() = default;
@@ -16,6 +34,7 @@ namespace Bruno
 
 	struct ModelComponent
 	{
-
+		UUID ModelHandle;
+		uint32_t SubmeshIndex{ 0 };
 	};
 }
