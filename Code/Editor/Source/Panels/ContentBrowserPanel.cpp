18a2998fdfbabc1e66086e39507695244664fa31
edgarbernal5 <edgar.bernal@gmail.com>
2024-01-27 00:02:42 -0300

    Populate directory tree and file list box. WIP tree and list boxes events

--- Code/Editor/Source/Panels/ContentBrowserPanel.cpp
+++ Code/Editor/Source/Panels/ContentBrowserPanel.cpp
@@ -1,6 +1,7 @@
 #include "ContentBrowserPanel.h"
 #include <filesystem>
 
+
 namespace Bruno
 {
 	ContentBrowserPanel::ContentBrowserPanel(nana::window window, const std::wstring& workingDirectory) : 
@@ -17,28 +18,74 @@ namespace Bruno
 
 		m_place["tree"] << m_treebox;
 		m_place["list"] << m_listbox;
+
+		m_listbox.append_header("Name", 150);
+
 		m_place.collocate();
 
-		PopulateDirectory(workingDirectory);
+		std::filesystem::directory_entry rootDirectoryEntry(m_workingDirectory);
+		auto node = m_treebox.insert("Content", "Content");
+		node.value(rootDirectoryEntry);
+		PopulateDirectory(node, workingDirectory);
+
+		m_treebox.events().selected([&](const nana::arg_treebox& arg)
+		{
+			m_listbox.auto_draw(false);
+			PopulateFileDirectory(arg.item);
+			m_listbox.auto_draw(true);
+		});
+
+		m_listbox.events().dbl_click([&](const nana::arg_mouse& args)
+		{
+			if (m_listbox.selected().size() == 0)
+				return;
+				
+			for (auto& item : m_listbox.selected()) {
+
+			}
+		});
+
+		m_fileSelectionPopup.append("Import asset", [](nana::menu::item_proxy& ip) {});
+		m_fileSelectionPopup.append_splitter();
+		m_fileSelectionPopup.append("Add to scene", [](nana::menu::item_proxy& ip) {});
+
+		m_listbox.events().mouse_down([&](const nana::arg_mouse& args)
+		{
+			if (m_listbox.selected().size() == 0)
+				return;
+
+			menu_popuper(m_fileSelectionPopup)(args);
+		});
 	}
 
-	void ContentBrowserPanel::PopulateDirectory(const std::wstring& directoryPath)
+	void ContentBrowserPanel::PopulateDirectory(nana::treebox::item_proxy& node, const std::wstring& directoryPath)
 	{
 		m_treebox.auto_draw(false);
-		PopulateDirectoryInner(directoryPath);
+		PopulateDirectoryInner(node, directoryPath);
 		m_treebox.auto_draw(true);
 	}
 
-	void ContentBrowserPanel::PopulateDirectoryInner(const std::wstring& directoryPath)
+	void ContentBrowserPanel::PopulateDirectoryInner(nana::treebox::item_proxy& node, const std::wstring& directoryPath)
 	{
-		for (const auto& dir : std::filesystem::directory_iterator{ directoryPath })
+		for (const auto& directoryEntry : std::filesystem::directory_iterator{ directoryPath })
 		{
-			if (!std::filesystem::is_directory(dir))
+			if (!std::filesystem::is_directory(directoryEntry))
 				continue;
 
-			//auto Path = m_treebox.make_key_path(arg.item, "/") + "/";
-			//m_treebox.insert(node, dir.path().filename().generic_string(), dir.path().filename().generic_string());
+			auto child = m_treebox.insert(node, directoryEntry.path().filename().generic_string(), directoryEntry.path().filename().generic_string());
+			child.value(directoryEntry);
+			PopulateDirectoryInner(child, directoryEntry.path());
+		}
+	}
 
+	void ContentBrowserPanel::PopulateFileDirectory(nana::treebox::item_proxy& node)
+	{
+		m_listbox.clear();
+		auto& nodeDirectoryEntry = node.value<std::filesystem::directory_entry>();
+		for (const auto& directoryEntry : std::filesystem::directory_iterator{ nodeDirectoryEntry.path()})
+		{
+			auto item = m_listbox.at(0).append(directoryEntry.path().filename(), true);
+			item.value(directoryEntry);
 		}
 	}
 }
--- Code/Editor/Source/Panels/ContentBrowserPanel.h
+++ Code/Editor/Source/Panels/ContentBrowserPanel.h
@@ -5,6 +5,7 @@
 #include <nana/gui/widgets/treebox.hpp>
 #include <nana/gui/widgets/listbox.hpp>
 
+#include <nana/gui/widgets/menu.hpp>
 namespace Bruno
 {
 	class ContentBrowserPanel : public nana::panel<true>
@@ -13,13 +14,16 @@ namespace Bruno
 		ContentBrowserPanel(nana::window window, const std::wstring& workingDirectory);
 
 	private:
-		void PopulateDirectory(const std::wstring& directory);
-		void PopulateDirectoryInner(const std::wstring& directory);
+		void PopulateDirectory(nana::treebox::item_proxy& node, const std::wstring& directory);
+		void PopulateDirectoryInner(nana::treebox::item_proxy& node, const std::wstring& directory);
+		void PopulateFileDirectory(nana::treebox::item_proxy& node);
 
 		std::wstring m_workingDirectory;
 		nana::place m_place;
 		nana::treebox m_treebox;
 		nana::listbox m_listbox;
+
+		nana::menu m_fileSelectionPopup;
 	};
 }
 
